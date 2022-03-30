101adf46f2683d3fc29dbd6a48a775f246c050a8
Manu Rúa <manuruaco@gmail.com>
2022-03-31 00:27:21 +0200

    Fix metadata Twitter

--- src/App.vue
+++ src/App.vue
@@ -23,46 +23,54 @@ useHead({
     {
       charset: "UTF-8",
     },
-    // {
-    //   property: "og:type",
-    //   content: "website"
-    // },
-    // {
-    //   property: "og:url",
-    //   content: "https://manurua.dev/"
-    // },
-    // {
-    //   property: "og:title",
-    //   content: "Manu Rúa | Desarrollo Backend con Golang, Web3, Gestión con Notion"
-    // },
-    // {
-    //   property: "og:description",
-    //   content: "Ingeniero software y creador digital en remoto desde Sevilla 💃🏻\nHablo sobre tecnologías Backend, Web3, Notion... y lo que se me vaya ocurriendo."
-    // },
-    // {
-    //   property: "og:image",
-    //   content: "/metaimage.svg"
-    // },
-    // {
-    //   property: "twitter:card",
-    //   content: "summary_large_image"
-    // },
-    // {
-    //   property: "twitter:url",
-    //   content: "https://manurua.dev/"
-    // },
-    // {
-    //   property: "twitter:title",
-    //   content: "Manu Rúa | Desarrollo Backend con Golang, Web3, Gestión con Notion"
-    // },
-    // {
-    //   property: "twitter:description",
-    //   content: "Ingeniero software y creador digital en remoto desde Sevilla 💃🏻\nHablo sobre tecnologías Backend, Web3, Notion... y lo que se me vaya ocurriendo."
-    // },
-    // {
-    //   property: "twitter:image",
-    //   content: "/metaimage.svg"
-    // },
+    {
+      property: "og:type",
+      content: "website"
+    },
+    {
+      property: "og:url",
+      content: "https://manurua.dev/"
+    },
+    {
+      property: "og:title",
+      content: "Manu Rúa | Desarrollo Backend con Golang, Web3, Gestión con Notion"
+    },
+    {
+      property: "og:description",
+      content: "Ingeniero software y creador digital en remoto desde Sevilla 💃🏻\nHablo sobre tecnologías Backend, Web3, Notion... y lo que se me vaya ocurriendo."
+    },
+    {
+      property: "og:image",
+      content: "https://manurua.dev/metaimage.svg"
+    },
+    {
+      property: "twitter:card",
+      content: "summary_large_image"
+    },
+    {
+      property: "twitter:url",
+      content: "https://manurua.dev/"
+    },
+    {
+      property: "twitter:title",
+      content: "Manu Rúa | Desarrollo Backend con Golang, Web3, Gestión con Notion"
+    },
+    {
+      property: "twitter:description",
+      content: "Ingeniero software y creador digital en remoto desde Sevilla 💃🏻\nHablo sobre tecnologías Backend, Web3, Notion... y lo que se me vaya ocurriendo."
+    },
+    {
+      property: "twitter:image:src",
+      content: "https://manurua.dev/metaimage.svg"
+    },
+    {
+      property: "twitter:image",
+      content: "<a href='https://manurua.dev/metaimage.svg'>https://manurua.dev/metaimage.svg</a>"
+    },
+    {
+      property: "twitter:creator",
+      content: "@manurua"
+    },
   ],
 })
 </script>
